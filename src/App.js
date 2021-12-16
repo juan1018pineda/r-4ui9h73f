f4d085860c6fabfa3e3b1bab85e1abcb2573d0ff
pineda.juan92 <pineda.juan92@gmail.com>
2021-12-15 21:06:01 -0500

    text stated created for the required task

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.write = this.write.bind(this);
+    this.state = {
+      text: "",
+    };
+  }
+
+  write(event) {
+    this.setState({
+      text: event.target.value,
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={this.write} />
+        <p className="repeater">{this.state.text}</p>
       </div>
     );
   }
